6535e8612c0423e5b448151a57e9db5f975fef08
jaina1008 <mailme.anshuljain@gmail.com>
2024-06-24 22:46:54 +1200

    created spark stream file

--- spark_stream.py
+++ spark_stream.py
@@ -1,6 +1,5 @@
 import logging
-from datetime import datetime
-from cassandra.auth import PlainTextAuthProvider
+
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
@@ -10,35 +9,35 @@ from pyspark.sql.types import StructType, StructField, StringType
 def create_keyspace(session):
     session.execute("""
         CREATE KEYSPACE IF NOT EXISTS spark_streams
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
     """)
 
-    print('Keyspace created successfully!')
+    print("Keyspace created successfully!")
 
 
 def create_table(session):
     session.execute("""
-    CREATE TABLE IF NOT EXISTS spark_streams.create_users (
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
         id UUID PRIMARY KEY,
         first_name TEXT,
         last_name TEXT,
         gender TEXT,
         address TEXT,
-        post_code TEXT, 
+        post_code TEXT,
         email TEXT,
         username TEXT,
         registered_date TEXT,
         phone TEXT,
         picture TEXT);
     """)
-    
+
     print("Table created successfully!")
 
 
 def insert_data(session, **kwargs):
     print("inserting data...")
 
-    id = kwargs.get('id')
+    user_id = kwargs.get('id')
     first_name = kwargs.get('first_name')
     last_name = kwargs.get('last_name')
     gender = kwargs.get('gender')
@@ -62,7 +61,6 @@ def insert_data(session, **kwargs):
 
     except Exception as e:
         logging.error(f'could not insert data due to {e}')
-    
 
 
 def create_spark_connection():
@@ -71,18 +69,17 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.41",
-                    "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
 
-        s_conn.sparkContext.setLogLevel('ERROR')
+        s_conn.sparkContext.setLogLevel("ERROR")
         logging.info("Spark connection created successfully!")
     except Exception as e:
-        logging.error("Couldn't create the spark connection due to exception {e}")
-    
-    return s_conn
+        logging.error(f"Couldn't create the spark session due to exception {e}")
 
+    return s_conn
 
 
 def connect_to_kafka(spark_conn):
@@ -101,16 +98,14 @@ def connect_to_kafka(spark_conn):
     return spark_df
 
 
-
 def create_cassandra_connection():
-
-    try: 
+    try:
         # connecting to the cassandra cluster
         cluster = Cluster(['localhost'])
+
         cas_session = cluster.connect()
 
         return cas_session
-    
     except Exception as e:
         logging.error(f"Could not create cassandra connection due to {e}")
         return None
@@ -138,7 +133,6 @@ def create_selection_df_from_kafka(spark_df):
     return sel
 
 
-
 if __name__ == "__main__":
     # create spark connection
     spark_conn = create_spark_connection()
@@ -156,9 +150,14 @@ if __name__ == "__main__":
             logging.info("Streaming is being started...")
 
             streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
-                               .option('checkpointLocation', '/tmp/checkppoint')
+                               .option('checkpointLocation', '/tmp/checkpoint')
                                .option('keyspace', 'spark_streams')
                                .option('table', 'created_users')
                                .start())
 
             streaming_query.awaitTermination()
+
+# docker compose up -d
+# docker logs data_engineering-db-1
+# docker exec -it cassandra cqlsh -u cassandra -p cassandra localhost 9042
+# describe spark_streams.created_users
