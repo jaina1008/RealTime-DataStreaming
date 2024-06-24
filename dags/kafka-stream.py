2aa96528abe9791f2f36529a80c4375adeb38a43
jaina1008 <mailme.anshuljain@gmail.com>
2024-06-24 18:40:12 +1200

    updated producer to broker from localhost

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -46,7 +46,7 @@ def stream_data():
     res = format_data(res)
     # print(json.dumps(res, indent=3))
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
 
     producer.send('users_created', json.dumps(res).encode('utf-8'))
 
